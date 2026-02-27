b02cb2f167888a04c13aba5cf70810fad22e9f95
asadali519 <alii.asad0519@gmail.com>
2026-02-27 23:26:12 +0500

    Removed Comments

--- l250519_Task1.cpp
+++ l250519_Task1.cpp
@@ -1,43 +1,43 @@
-////Asad Ali
-////25l-0519
-//
-//
-//#include <iostream>
-//
-//using namespace std;
-//
-//int findMax(int* arr, int size);
-//
-//int main() {
-//    int n;
-//    cout << "Enter the Size of Array: ";
-//    cin >> n;
-//
-//    
-//    int* arr = new int[n];
-//
-//    for (int i = 0; i < n; i++) {
-//        cout << "Enter Element " << i + 1 << ": ";
-//        cin >> *(arr + i);
-//    }
-//
-//    int max = findMax(arr, n);
-//    cout << "The maximum element is: " << max << endl;
-//
-//    delete[] arr; 
-//
-//    return 0;
-//}
-//
-//
-//int findMax(int* arr, int size) {
-//    int max = *(arr + 0);
-//  
-//    for (int* ptr = arr; ptr < arr + size; ptr++) {
-//        if (*ptr > max) {
-//            max = *ptr;
-//        }
-//    }
-//
-//    return max;
-//}
+//Asad Ali
+//25l-0519
+
+
+#include <iostream>
+
+using namespace std;
+
+int findMax(int* arr, int size);
+
+int main() {
+   int n;
+   cout << "Enter the Size of Array: ";
+   cin >> n;
+
+   
+   int* arr = new int[n];
+
+   for (int i = 0; i < n; i++) {
+       cout << "Enter Element " << i + 1 << ": ";
+       cin >> *(arr + i);
+   }
+
+   int max = findMax(arr, n);
+   cout << "The maximum element is: " << max << endl;
+
+   delete[] arr; 
+
+   return 0;
+}
+
+
+int findMax(int* arr, int size) {
+   int max = *(arr + 0);
+ 
+   for (int* ptr = arr; ptr < arr + size; ptr++) {
+       if (*ptr > max) {
+           max = *ptr;
+       }
+   }
+
+   return max;
+}
--- l250519_Task4.cpp
+++ l250519_Task4.cpp
@@ -1,104 +1,104 @@
-////Asad Ali
-////25l-0519
-//
-//#include <iostream>
-//using namespace std;
-//
-//void resizeArray(int*& arr, int& currentSize, int newSize);
-//void printArray(int* arr, int size);
-//
-//int main() {
-//    int initialSize;
-//    cout << "Enter initial size of the array: ";
-//    cin >> initialSize;
-//    if (initialSize <= 0) {
-//        cout << "Initial size must be positive." << endl;
-//        return 1;
-//    }
-//    int* dynamicArray = new int[initialSize];
-//    int currentSize = initialSize;
-//
-//    for (int i = 0; i < currentSize; i++) {
-//        cout << "Element " << i + 1 << ": ";
-//        cin >> *(dynamicArray + i);
-//    }
-//    int choice, newSize;
-//    cout << "Enter Choice: ";
-//    
-//
-//    cout << "1 for Grow \n2 for Shrink \n3 to Exit" << endl;
-//
-//    cin >> choice;
-//
-//    if (choice == 1) {
-//        cout << "Enter new larger size: ";
-//        cin >> newSize;
-//        if (newSize > currentSize) {
-//            resizeArray(dynamicArray, currentSize, newSize);
-//        }
-//        else {
-//            cout << "New size must be larger than the current size to grow." << endl;
-//        }
-//    }
-//    else if (choice == 2) {
-//        cout << "Enter new smaller size: ";
-//        cin >> newSize;
-//        if (newSize < currentSize && newSize > 0) {
-//            resizeArray(dynamicArray, currentSize, newSize);
-//        }
-//        else {
-//            cout << "New size must be smaller than the current size and positive to shrink." << endl;
-//        }
-//    }
-//    else if (choice == 3) {
-//        cout << "Printed Array: " << endl;
-//        printArray(dynamicArray, currentSize);
-//    }
-//    else if (choice == 4) {
-//        delete[] dynamicArray;
-//        return 0;
-//    }
-//
-//    return 0;
-//}
-//
-//void resizeArray(int*& arr, int& currentSize, int newSize) {
-//    if (newSize <= 0) {
-//        cout << "New size must be positive." << endl;
-//        return;
-//    }
-//    int elementsToCopy = 0;
-//    int* newArr = new int[newSize];
-//    if (currentSize < newSize) {
-//        elementsToCopy = currentSize;
-//    }
-//    else if (newSize < currentSize) {
-//        elementsToCopy = newSize;
-//    }
-//
-//    for (int i = 0; i < elementsToCopy; i++) {
-//        *(newArr + i) = *(arr + i);
-//    }
-//    if (newSize > currentSize) {
-//        cout << "Enter " << newSize - currentSize << " new integer elements:" << endl;
-//        for (int i = currentSize; i < newSize; i++) {
-//            cout << "Element [" << i + 1 << "]: ";
-//            cin >> *(newArr + i);
-//        }
-//        cout << endl;
-//    }
-//    delete[] arr;
-//    arr = newArr;
-//    currentSize = newSize;
-//    cout << "Array successfully resized to " << currentSize << " elements." << endl;
-//
-//    cout << "Printed Array: " << endl;
-//    printArray(newArr, currentSize);
-//}
-//
-//void printArray(int* arr, int size) {
-//    for (int i = 0; i < size; i++) {
-//        cout << *(arr + i) << " ";
-//    }
-//    cout << endl;
-//}
+//Asad Ali
+//25l-0519
+
+#include <iostream>
+using namespace std;
+
+void resizeArray(int*& arr, int& currentSize, int newSize);
+void printArray(int* arr, int size);
+
+int main() {
+   int initialSize;
+   cout << "Enter initial size of the array: ";
+   cin >> initialSize;
+   if (initialSize <= 0) {
+       cout << "Initial size must be positive." << endl;
+       return 1;
+   }
+   int* dynamicArray = new int[initialSize];
+   int currentSize = initialSize;
+
+   for (int i = 0; i < currentSize; i++) {
+       cout << "Element " << i + 1 << ": ";
+       cin >> *(dynamicArray + i);
+   }
+   int choice, newSize;
+   cout << "Enter Choice: ";
+   
+
+   cout << "1 for Grow \n2 for Shrink \n3 to Exit" << endl;
+
+   cin >> choice;
+
+   if (choice == 1) {
+       cout << "Enter new larger size: ";
+       cin >> newSize;
+       if (newSize > currentSize) {
+           resizeArray(dynamicArray, currentSize, newSize);
+       }
+       else {
+           cout << "New size must be larger than the current size to grow." << endl;
+       }
+   }
+   else if (choice == 2) {
+       cout << "Enter new smaller size: ";
+       cin >> newSize;
+       if (newSize < currentSize && newSize > 0) {
+           resizeArray(dynamicArray, currentSize, newSize);
+       }
+       else {
+           cout << "New size must be smaller than the current size and positive to shrink." << endl;
+       }
+   }
+   else if (choice == 3) {
+       cout << "Printed Array: " << endl;
+       printArray(dynamicArray, currentSize);
+   }
+   else if (choice == 4) {
+       delete[] dynamicArray;
+       return 0;
+   }
+
+   return 0;
+}
+
+void resizeArray(int*& arr, int& currentSize, int newSize) {
+   if (newSize <= 0) {
+       cout << "New size must be positive." << endl;
+       return;
+   }
+   int elementsToCopy = 0;
+   int* newArr = new int[newSize];
+   if (currentSize < newSize) {
+       elementsToCopy = currentSize;
+   }
+   else if (newSize < currentSize) {
+       elementsToCopy = newSize;
+   }
+
+   for (int i = 0; i < elementsToCopy; i++) {
+       *(newArr + i) = *(arr + i);
+   }
+   if (newSize > currentSize) {
+       cout << "Enter " << newSize - currentSize << " new integer elements:" << endl;
+       for (int i = currentSize; i < newSize; i++) {
+           cout << "Element [" << i + 1 << "]: ";
+           cin >> *(newArr + i);
+       }
+       cout << endl;
+   }
+   delete[] arr;
+   arr = newArr;
+   currentSize = newSize;
+   cout << "Array successfully resized to " << currentSize << " elements." << endl;
+
+   cout << "Printed Array: " << endl;
+   printArray(newArr, currentSize);
+}
+
+void printArray(int* arr, int size) {
+   for (int i = 0; i < size; i++) {
+       cout << *(arr + i) << " ";
+   }
+   cout << endl;
+}
